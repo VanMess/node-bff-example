0956ab2ca5c66b76157e3c79411cec3f45e03e3b
范文杰 <wenfan@malong.com>
2019-11-26 20:36:55 +0800

    docs: add comments

--- src/web/routers/index.js
+++ src/web/routers/index.js
@@ -1,6 +1,9 @@
 import Router from 'koa-router';
 import PingRouter from './ping';
 
+/*
+ * Merge all routers in one place.
+ */
 export default new Router()
   // Registe ping routers
   .use(PingRouter.routes());
