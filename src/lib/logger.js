c41933f866ac67d7ad9691b589646dd0146a28fb
范文杰 <wenfan@malong.com>
2019-11-28 20:36:39 +0800

    docs: add comments about logging handler

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,4 +1,9 @@
+// https://www.npmjs.com/package/bristol
+// ! good support for mutil output
 import { Bristol } from 'bristol';
+
+// www.npmjs.com/package/palin
+// ! A beautiful Bristol formatter
 import palin from 'palin';
 
 const logger = new Bristol();
