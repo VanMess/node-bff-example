79ea553e899e6bdb860fe2474c9b0360a747b423
范文杰 <wenfan@malong.com>
2019-11-30 19:19:54 +0800

    chore: more config session for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,12 +9,17 @@ module.exports = {
       name: 'node-bff-example',
       script: 'dist/bin/www.js',
       watch: false,
+      cwd: './',
+      // 启动进程数
       instances: 4,
+      // 执行模式，
       exec_mode: 'cluster',
       max_memory_restart: '300M',
       env_production: {
         NODE_ENV: 'production'
-      }
+      },
+      error_file: './logs/err.log', // 错误日志路径
+      out_file: './logs/out.log'
     }
   ]
 };
